be1dc9f3dee32aa7fd084a750884654eda43ecd9
dangtong76 <dangtong@gmail.com>
2025-03-07 16:27:44 +0900

    init

--- scripts/after_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod -R 755 /usr/share/nginx/html
-chown -R nginx:nginx /usr/share/nginx/html
-systemctl restart nginx
--- scripts/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [ -d /usr/share/nginx/html ]; then
-    rm -rf /usr/share/nginx/html/*
-fi
